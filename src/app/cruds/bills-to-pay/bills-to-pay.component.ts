387c1d4f1e4ac4bdb5b626b3ca53bc4144d597bc
Matheus Souto <frameworktecnologia37@localiza.com>
2023-10-24 21:04:26 -0300

    ✨feature: Crud contas a pagar parte 1

--- src/app/cruds/bills-to-pay/bills-to-pay.component.ts
+++ src/app/cruds/bills-to-pay/bills-to-pay.component.ts
@@ -3,28 +3,96 @@ import { Product } from 'src/app/demo/api/product';
 import { MessageService } from 'primeng/api';
 import { Table } from 'primeng/table';
 import { ProductService } from 'src/app/demo/service/product.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BillToPay } from 'src/app/interfaces/bill-to-pay';
+import { BillsToPayService } from 'src/app/services/bills-to-pay.service';
+import { map } from 'rxjs';
 
 @Component({
     templateUrl: './bills-to-pay.component.html',
     providers: [MessageService]
 })
 export class BillsToPayComponent implements OnInit {
-    cols: any[] = [];
-    rowsPerPageOptions = [5, 10, 20];
+    public cols: any[] = [];
+    public rowsPerPageOptions = [5, 10, 20];
+    public form!: FormGroup;
+    public items: BillToPay[] = [];
+    public item!: BillToPay;
+    public itemDialog: boolean = false;
+    public deleteItemDialog: boolean = false;
 
-    constructor(private productService: ProductService, private messageService: MessageService) { }
+    constructor(
+        private productService: ProductService,
+        private messageService: MessageService,
+        private billsToPayService: BillsToPayService,
+        private formBuilder: FormBuilder
+    ) { }
 
     ngOnInit() {
     }
 
     openNew() {
+        this.itemDialog = true;
+        this.form.reset();
     }
 
     hideDialog() {
-
+        this.itemDialog = false;
     }
 
     onGlobalFilter(table: Table, event: Event) {
         table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
     }
+
+    onCreateForm() {
+       this.form = this.formBuilder.group({
+          name: ['', Validators.required],
+          documentDate: ['', Validators.required],
+          documentNumber: ['', Validators.required],
+          supplierName: ['', Validators.required],
+          amount: ['', Validators.required],
+          installmentQuantity: ['', Validators.required],
+          dueDate: ['', Validators.required]
+       });
+    }
+
+    onLoadItems() {
+        this.billsToPayService.getAll().snapshotChanges().pipe(
+            map(changes =>
+               changes.map(c =>
+                ({ id: c.payload.doc.id, ...c.payload.doc.data()})
+               )
+            )
+        ).subscribe(data => {
+            this.items = data;
+        });
+    }
+
+    onSaveForm() {
+        if (!this.item.id) {
+            return this.createBillPay();
+        }
+
+        return this.updateBillPay(this.item.id);
+    }
+
+    createBillPay() {
+        this.billsToPayService.create(this.form.value).then(() => {
+            this.itemDialog = false;
+            this.form.reset();
+        })
+    }
+
+    updateBillPay(id: string) {
+        this.billsToPayService.update(id, this.form.value).then(res => {
+            this.itemDialog = false;
+            this.form.reset();
+        })
+    }
+
+    deleteBillPay(billPay: BillToPay) {
+        this.deleteItemDialog = true;
+        this.item = billPay;
+    }
+
 }
